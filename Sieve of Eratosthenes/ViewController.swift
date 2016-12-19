0d843eeacb3674e04add809ea5dd8094ef7f6797
Ethan Schoen <e.schoen@yahoo.com>
2016-12-19 15:11:43 -0500

    Primes can be found n numbers now

--- Sieve of Eratosthenes/ViewController.swift
+++ Sieve of Eratosthenes/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func get_n_primes(n: Int) -> [Bool]{
+        var naturals = [Bool](repeating: true, count: n) // true means prime, start with all true
+        // 0 and 1 are not prime
+        naturals[0] = false
+        naturals[1] = false
+        for i in 2..<n {
+            if naturals[i] {
+                var j = i;
+                while j*i < n {
+                    naturals[j*i] = false
+                    j += 1
+                }
+            }
+        }
+        for i in 0..<n {
+            if naturals[i] {
+                print(i, "is a prime")
+            }
+        }
+        return naturals
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        get_n_primes(n: 100)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
